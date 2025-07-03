f545a7f89e372b262b307e579d64ea9b79d5162a
meetjain <jainmeet162003@gmail.com>
2025-07-04 02:06:32 +0530

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
